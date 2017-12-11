9d9f05c5f38c1a7b34515750e781e34b3b91890d
Damon <thydamon@gmail.com>
2017-12-11 10:07:21 -0500

    Insert Sort

--- Sort.cpp
+++ Sort.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 using namespace std;
 
+int PrintSort(int arr[],int n);
+
 int BubbleSort(int arr[], int n)
 {
 	for (int i = 0; i < n; i++)
@@ -51,6 +53,44 @@ int SelectSort(int arr[], int n)
 	return 0;
 }
 
+int InsertSort(int arr[], int n)
+{
+	int i,j;
+#if 0
+	for (i = 0; i < n; i++)
+	{
+		if (arr[i-1] > arr[i])
+		{
+			int tmp = arr[i];
+
+			for (j = i-1; arr[j]>tmp && j >= 0; j--)
+			{
+				arr[j+1] = arr[j];
+			}
+
+			arr[j+1] = tmp;
+		}
+	}
+#endif
+
+	for (i = 0; i < n-1; i++)
+	{
+		if (arr[i] > arr[i+1])
+		{
+			int tmp = arr[i+1];
+
+			for (j = i; arr[j] > tmp && j >= 0; j--)
+			{
+				arr[j+1] = arr[j];
+			}
+
+			arr[j+1] = tmp;
+		}
+	}
+
+	return 0;
+}
+
 int PrintSort(int arr[],int n)
 {
     for (int i = 0; i < n; i++)
@@ -68,7 +108,8 @@ int main()
 	int arr[10] = {1,3,5,4,2,7,8,9,0,6};
 
 	// BubbleSort(arr,10);
-	SelectSort(arr,10);
+	// SelectSort(arr,10);
+	InsertSort(arr,10);
 	PrintSort(arr,10);
   
 	return 0;
